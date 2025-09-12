4972c926b0e0d9d46254c1d5ef1bb4d6c86f62dd
Kevin Nunez <nune0042@algonquinlive.com>
2025-09-12 12:01:58 -0400

    Finished destructor

--- week2.cpp
+++ week2.cpp
@@ -11,12 +11,19 @@ namespace CST8219 {
 		Vehicle(int w, int d) {
 			numWheels = w;
 			numDoors = d;
+			cout << "In constructor with 2 parameters" << endl;
 		}
 
 		Vehicle(int w) : Vehicle(w, 4) {
+			cout << "In constructor with 1 parameter, wheels = " << w << endl;
 		}
 
 		Vehicle() : Vehicle(4) {
+			cout << "In constructor with 0 parameters" << endl;
+		}
+
+		~Vehicle() {
+			cout << "In destructor" << endl;
 		}
 	};
 }
