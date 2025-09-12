440b7e8bf3d7334e4146e2f7ec91a90e7b9b661c
Kevin Nunez <nune0042@algonquinlive.com>
2025-09-12 12:01:30 -0400

    Finished constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,26 @@
-
 #include<iostream>
 
-int main(int argc, char **argv)
-{
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+using namespace std;
+
+namespace CST8219 {
+	class Vehicle {
+	private:
+		int numWheels;
+		int numDoors;
+	public:
+		Vehicle(int w, int d) {
+			numWheels = w;
+			numDoors = d;
+		}
+
+		Vehicle(int w) : Vehicle(w, 4) {
+		}
+
+		Vehicle() : Vehicle(4) {
+		}
+	};
+}
+
+int main(int argc, char** argv) {
+	return 0;
 }
