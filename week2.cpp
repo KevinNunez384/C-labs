9183d4c5cf522f4591bd25f2d8317e7e955b8200
Kevin Nunez <nune0042@algonquinlive.com>
2025-10-02 14:06:54 -0400

    Completed Lab 4: Copy Constructors and File Separation

--- week2.cpp
+++ week2.cpp
@@ -1,69 +1,26 @@
-#include<iostream>
-
+#include "Vehicle.h"
 using namespace std;
 
-namespace CST8219 {
-	class Vehicle {
-	private:
-		int numWheels;
-		int numDoors;
-	public:
-		Vehicle(int w, int d) {
-			numWheels = w;
-			numDoors = d;
-			cout << "In constructor with 2 parameters" << endl;
-		}
-
-		Vehicle(int w) : Vehicle(w, 4) {
-			cout << "In constructor with 1 parameter, wheels = " << w << endl;
-		}
-
-		Vehicle() : Vehicle(4) {
-			cout << "In constructor with 0 parameters" << endl;
-		}
-
-		~Vehicle() {
-			cout << "In destructor" << endl;
-		}
-
-		void display() const {
-			cout << "Vehicle has " << numDoors << " doors and " << numWheels << " wheels." << endl;
-		}
-	};
+void CreateVehicle(Vehicle& v, int w = 4, int d = 2) {
+    v.setWheels(w);
+    v.setDoors(d);
 }
 
 int main(int argc, char** argv) {
-	CST8219::Vehicle v1;
-	CST8219::Vehicle v2(4, 2);
-	cout << "Size of v1: " << sizeof(v1) << " bytes\n";
-	cout << "Size of v2: " << sizeof(v2) << " bytes\n";
+    Vehicle original;              
+    Vehicle copy(original);        
+    Vehicle secondCopy(&original); 
 
-	CST8219::Vehicle* pVehicle = nullptr;
+    copy.printVehicle();
 
-	char choice;
+    CreateVehicle(copy, 2);        
+    copy.printVehicle();
 
-	do {
-		int d, w;
-		cout << "Enter number of doors (>0): ";
-		cin >> d;
-		cout << "Enter number of wheels (>0): ";
-		cin >> w;
+    CreateVehicle(copy, 2, 3);     
+    copy.printVehicle();
 
-		if (d > 0 && w > 0) {
-			delete pVehicle;
-			pVehicle = new CST8219::Vehicle(w, d);
-			pVehicle->display();
-			cout << "Size of pointer pVehicle: " << sizeof(pVehicle) << " bytes\n";
+    copy = secondCopy;            
+    copy.printVehicle();
 
-		} else {
-			cout << "Invalid input, please try again.\n";
-		}
-
-		cout << "Do you want to create another vehicle? (Y/N)";
-		cin >> choice;
-	} while (choice == 'y' || choice == 'Y');
-
-	delete pVehicle;
-
-	return 0;
+    return 0;
 }
