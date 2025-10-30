c4b456151b847f98a5077a40f9234cdf75ae1cf0
Kevin Nunez <nune0042@algonquinlive.com>
2025-10-30 14:19:03 -0400

    Actual Lab 5 completed

--- week2.cpp
+++ week2.cpp
@@ -1,4 +1,5 @@
 #include "Vehicle.h"
+#include <iostream>
 using namespace std;
 
 void CreateVehicle(Vehicle& v, int w = 4, int d = 2) {
@@ -6,21 +7,52 @@ void CreateVehicle(Vehicle& v, int w = 4, int d = 2) {
     v.setDoors(d);
 }
 
-int main(int argc, char** argv) {
-    Vehicle original;
+//Optional C++23 feature
+consteval int compileTimeValue() { return 42; }
+int calculate() {
+    if consteval {
+        return compileTimeValue();
+    }
+    else {
+        return 24;
+    }
+}
+
+int main() {
+    cout << "Active vehicles: " << Vehicle::getVehicleCount() << endl;
+
+    Vehicle original(4, 2, "Sedan");
     Vehicle copy(original);
     Vehicle secondCopy(&original);
 
-    copy.printVehicle();
+    cout << original << endl;
+    cout << copy << endl;
 
     CreateVehicle(copy, 2);
-    copy.printVehicle();
+    cout << "After changing wheels: " << copy << endl;
 
     CreateVehicle(copy, 2, 3);
-    copy.printVehicle();
+    cout << "After changing wheels & doors: " << copy << endl;
 
     copy = secondCopy;
-    copy.printVehicle();
+    cout << "After assignment: " << copy << endl;
+
+    cout << "Comparison operators demo: "
+        << (copy == original ? "Equal" : "Not Equal") << endl;
+
+    cout << "Increment / Decrement demo:" << endl;
+    ++copy;
+    cout << "Prefix ++: " << copy << endl;
+    copy++;
+    cout << "Postfix ++: " << copy << endl;
+    --copy;
+    cout << "Prefix --: " << copy << endl;
+    copy--;
+    cout << "Postfix --: " << copy << endl;
+
+    cout << "Total Vehicle objects created: " << Vehicle::getVehicleCount() << endl;
+
+    cout << "C++23 if consteval demo: " << calculate() << endl;
 
     return 0;
 }
